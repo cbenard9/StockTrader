6ada091c108ba9ef96b62c9b0609d031bd264831
Chris Benard <chrisbenard@mail.weber.edu>
2019-12-12 22:10:46 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container">
+        
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
+                <transition name="slide" mode="out-in">
+                   
+                    <router-view></router-view>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+    
+
+    export default {
+        components: {
+        appHeader: Header
+        },
+        
+        created() {
+            this.$store.dispatch('initStocks')
+        }
+        
+    }
+</script>
+
+<style>
+    body {
+        padding: 30px;
+    }
+
+.slide-enter-active {
+    animation: slide-in 2s ease-out forwards;
+}
+.slide-leave-active {
+    transition: opacity 2s ease;
+    opacity: 0;
+    animation: slide-in 1s ease-out forwards;
+}
+
+.slide-leave {
+    opacity: 1;
+    transform: translateX(0);
+}
+   
+@keyframes slide-in {
+    from {
+        transform: translateY(0);
+        opacity: 0;
+        transform: scale(1.1);
+        
+    }
+    to {
+        transform: translateY(-45px);
+        opacity: 1;
+        transform: scale(1);
+        
+    }
+}
+
+@keyframes slide-out {
+    from {
+        transform: translateY(-45px);
+        opacity: 1;
+        transform: scale(1); 
+    }
+     to {
+        transform: translateY(0);
+        opacity: 0;
+        transform: scale(1.1); 
+     }
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,30 @@
+
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import { routes } from'./routes.js'
+import store from './store/store'
+
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+
+Vue.http.options.root = 'https://stock-ba58a.firebaseio.com/' 
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString()
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
